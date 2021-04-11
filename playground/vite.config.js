b1dea4be33b819f4b682145455814c440fed948e
yaquawa <yaquawa@gmail.com>
2021-04-11 13:53:53 +0900

    update playground

--- playground/vite.config.js
+++ playground/vite.config.js
@@ -1,7 +1,7 @@
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig, loadEnv } from 'vite'
-import ViewLauncherVuePlugin from '@view-launcher/rollup-plugin'
+import ViewLauncherVuePlugin from '@view-launcher/rollup-plugin-vue'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, __dirname)
