4ad85fb8a29c0e5ec2a7a2527feb9f534e468d14
Kamil Bielak <kamil.bielak@symfonia.pl>
2023-01-14 21:23:12 +0100

    vscode-remote

--- src/editorUrl.ts
+++ src/editorUrl.ts
@@ -7,7 +7,7 @@ const EDITORS = {
   webstorm: 'phpstorm://open?file={path}&line={line}&column={column}',
   idea: 'idea://open?file={path}&line={line}&column={column}',
   vscode: 'vscode://file/{path}:{line}:{column}',
-  'vscode-wsl': 'vscode://', //vscode://vscode-remote/wsl+distro/path/to/file
+  'vscode-remote': 'vscode://vscode-remote/{path}:{line}:{column}',
   'vscode-insiders': 'vscode-insiders://file/{path}:{line}:{column}',
   atom: 'atom://core/open/file?filename={path}&line={line}&column={column}',
 }
